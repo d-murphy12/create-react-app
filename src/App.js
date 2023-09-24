d00bb1fc80a368f07ad0bd9e10603edc19aa72db
d-murphy12 <d12murph@gmail.com>
2023-09-24 16:56:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
     
-    <div id="card" className="card">
+    <div id="card" class="card">
         <img src="https://media.istockphoto.com/id/1072291024/vector/vector-illustration-cartoon-residential-townhouse.jpg?s=612x612&w=0&k=20&c=vhYukmGJvZYdabADEakKi8W2-V47XzZ_fUW-3RRmPjE=" width="100"
        height="100"/>
        <h3 >Real Estate Agency</h3>
     <h4>IST 256 Pod 2</h4>
-    <details className="details">
+    <details class="details">
     <summary>Open me</summary>
     <div>
     The best agency around! Give us a call!
