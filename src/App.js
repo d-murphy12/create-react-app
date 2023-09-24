71ceefde06ca7dd054b10ca45668c65c259bdcc3
d-murphy12 <d12murph@gmail.com>
2023-09-24 16:55:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div id="card" className="card">
+        <img src="https://media.istockphoto.com/id/1072291024/vector/vector-illustration-cartoon-residential-townhouse.jpg?s=612x612&w=0&k=20&c=vhYukmGJvZYdabADEakKi8W2-V47XzZ_fUW-3RRmPjE=" width="100"
+       height="100"/>
+       <h3 >Real Estate Agency</h3>
+    <h4>IST 256 Pod 2</h4>
+    <details className="details">
+    <summary>Open me</summary>
+    <div>
+    The best agency around! Give us a call!
     </div>
+    </details>
+</div>
+
   );
 }
 
