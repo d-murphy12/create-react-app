6aecfd033e6f5e0bae83ed0535ad6d3207c02dff
d-murphy12 <d12murph@gmail.com>
2023-09-24 17:00:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    
+    <div class="buttons">
+      <button class="duplicate">Duplicate card</button>
+      <button class="changetxt">Change title</button>
+<button id="deletecard">Delete</button>
+<button data-toggle-btn>Toggle details</button>
+  <button id="changebackground">Change Background</button>
+  </div>
+
     <div id="card" class="card">
         <img src="https://media.istockphoto.com/id/1072291024/vector/vector-illustration-cartoon-residential-townhouse.jpg?s=612x612&w=0&k=20&c=vhYukmGJvZYdabADEakKi8W2-V47XzZ_fUW-3RRmPjE=" width="100"
        height="100"/>
